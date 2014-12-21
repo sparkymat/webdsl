b5e3b054f7549033e7504af693db9c0f2bde47ee
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-12-22 08:56:56 +1100

    Shortcut for styling class based selectors

--- css/class.go
+++ css/class.go
@@ -5,3 +5,7 @@ type Class string
 func (class Class) Selector() string {
 	return "." + string(class)
 }
+
+func (class Class) Style(properties ...Property) RuleSet {
+	return Rule().For(class).Set(properties...)
+}
