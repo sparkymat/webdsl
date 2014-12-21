6165c06480ec3195bd8dafaebdb2eef50bade3a8
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-12-21 11:04:00 +1100

    Migrated names colors to color interface

---
+++ css/color/standard_colors.go
@@ -0,0 +1,27 @@
+package color
+
+type namedColor string
+
+const Initial namedColor = "initial"
+const Inherit namedColor = "inherit"
+const Aqua namedColor = "aqua"
+const Black namedColor = "black"
+const Blue namedColor = "blue"
+const Fuchsia namedColor = "fuchsia"
+const Gray namedColor = "gray"
+const Green namedColor = "green"
+const Lime namedColor = "lime"
+const Maroon namedColor = "maroon"
+const Navy namedColor = "navy"
+const Olive namedColor = "olive"
+const Orange namedColor = "orange"
+const Purple namedColor = "purple"
+const Red namedColor = "red"
+const Silver namedColor = "silver"
+const Teal namedColor = "teal"
+const White namedColor = "white"
+const Yellow namedColor = "yellow"
+
+func (color namedColor) ColorString() string {
+	return string(color)
+}
