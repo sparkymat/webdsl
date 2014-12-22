8ab0147bdd7c8b40ff8855328f6a259fd66bacf8
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-12-23 09:20:07 +1100

    Got rid of halfbaked rawselector

--- css/selector.go
+++ css/selector.go
@@ -3,14 +3,3 @@ package css
 type Selector interface {
 	Selector() string
 }
-
-//TODO bad name
-type RawSelector string
-
-func (selector RawSelector) Selector() string {
-	return string(selector)
-}
-
-func (selector RawSelector) Style(properties ...Property) RuleSet {
-	return Rule().For(selector).Set(properties...)
-}
