5561a3ca892a8e246ca5ea65deef1331af7ca4df
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2014-12-22 20:28:06 +1100

    More Flexbox stuff

--- css/property_display.go
+++ css/property_display.go
@@ -1,5 +1,7 @@
 package css
 
+import "fmt"
+
 type displayType string
 
 //TODO Feel like display should be own package
@@ -8,6 +10,36 @@ const Flex displayType = "flex"
 
 func Display(value displayType) Property {
 	property := Property{propertyType: "display"}
+	property.values = append(property.values, string(value))
+
+	return property
+}
+
+type flexDirection string
+
+const Column flexDirection = "column"
+const Row flexDirection = "row"
+
+func FlexDirection(value flexDirection) Property {
+	property := Property{propertyType: "flex-direction"}
+	property.values = append(property.values, string(value))
+
+	return property
+}
+
+func FlexGrow(value int) Property {
+	property := Property{propertyType: "flex-grow"}
+	property.values = append(property.values, fmt.Sprintf("%v", value))
+
+	return property
+}
+
+type itemAlign string
+
+const Center itemAlign = "center"
+
+func AlignItems(value itemAlign) Property {
+	property := Property{propertyType: "align-items"}
 	property.values = append(property.values, string(value))
 
 	return property
