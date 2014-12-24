7817c268ea6ded9c69b8a771504e4fd1c4c17de1
Ajith Hussain <csy0013@googlemail.com>
2014-12-24 11:21:00 +1100

    Better testing for generated forms`

--- rest/resource.go
+++ rest/resource.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 
@@ -13,10 +14,6 @@ type Resource struct {
 	object interface{}
 }
 
-func (resource Resource) Form(action Action) html.Node {
-	return html.Div()
-}
-
 func (resource Resource) Name() string {
 	valueReflection := reflect.ValueOf(resource.object)
 	fullyQualifiedType := valueReflection.Type().String()
@@ -27,4 +24,37 @@ func (resource Resource) Name() string {
 	pluralizedResourceName := inflector.Pluralize(resourceName)
 
 	return pluralizedResourceName
+}
+
+func (resource Resource) FormFor(action Action) html.Node {
+	return resource.FormForWithWrappedInputs(action, func(inputNode html.Node) html.Node {
+		return inputNode
+	})
+}
+
+func (resource Resource) FormForWithWrappedInputs(action Action, wrapper func(html.Node) html.Node) html.Node {
+	var inputNodes []html.Node
+	innerValue := reflect.ValueOf(resource.object)
+	valueType := innerValue.Type()
+
+	if valueType.Kind() != reflect.Struct {
+		panic("Not a struct resource")
+	}
+
+	for i := 0; i < innerValue.NumField(); i++ {
+		field := innerValue.Field(i)
+
+		resourceName := resource.Name()
+		fieldName := util.CamelToUnderscore(valueType.Field(i).Name)
+		inputName := fmt.Sprintf("%v[%v]", inflector.Singularize(resourceName), fieldName)
+		inputNode := html.Input().Name(inputName).Value(fmt.Sprintf("%v", field.Interface()))
+
+		wrappedNode := wrapper(inputNode)
+		inputNodes = append(inputNodes, wrappedNode)
+	}
+
+	submitNode := html.Input().Type("submit").Value(strings.Title(string(action)))
+	inputNodes = append(inputNodes, submitNode)
+
+	return html.Form().Children(inputNodes...)
 }
--- rest/resource_test.go
+++ rest/resource_test.go
@@ -1,6 +1,12 @@
 package rest
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/kirillrdy/nadeshiko/html"
+	"github.com/sparkymat/webdsl/css"
+)
 
 type Car struct {
 	Id         uint32
@@ -15,5 +21,44 @@ func TestResourceNameGeneration(t *testing.T) {
 
 	if resource.Name() != "cars" {
 		t.Errorf("Name generation failed: Expected: cars. Got: %v.", resource.Name())
+	}
+
+	expectedForm := html.Form().Children(
+		html.Input().Name("car[id]").Value("5"),
+		html.Input().Name("car[make]").Value("Honda"),
+		html.Input().Name("car[model]").Value("Civic"),
+		html.Input().Name("car[wheel_count]").Value("4"),
+		html.Input().Type("submit").Value("Create"),
+	)
+	generatedForm := resource.FormFor(Create)
+
+	if reflect.DeepEqual(expectedForm, generatedForm) != true {
+		t.Error("Expected: ", expectedForm.String())
+		t.Error("Got: ", generatedForm.String())
+	}
+
+	expectedForm = html.Form().Children(
+		html.Div().Class(css.Class("form-input")).Children(
+			html.Input().Name("car[id]").Value("5"),
+		),
+		html.Div().Class(css.Class("form-input")).Children(
+			html.Input().Name("car[make]").Value("Honda"),
+		),
+		html.Div().Class(css.Class("form-input")).Children(
+			html.Input().Name("car[model]").Value("Civic"),
+		),
+		html.Div().Class(css.Class("form-input")).Children(
+			html.Input().Name("car[wheel_count]").Value("4"),
+		),
+		html.Input().Type("submit").Value("Create"),
+	)
+
+	generatedForm = resource.FormForWithWrappedInputs(Create, func(inputNode html.Node) html.Node {
+		return html.Div().Class(css.Class("form-input")).Children(inputNode)
+	})
+
+	if reflect.DeepEqual(expectedForm, generatedForm) != true {
+		t.Error("Expected: ", expectedForm.String())
+		t.Error("Got: ", generatedForm.String())
 	}
 }
