4be1e9fdcd0ccdb8983aee3897819a8056e1e67b
Ajith Hussain <csy0013@googlemail.com>
2015-02-20 22:04:17 +1100

    Added inner and variant helpers for Class

--- css/class.go
+++ css/class.go
@@ -1,5 +1,7 @@
 package css
 
+import "fmt"
+
 type Class string
 
 func (class Class) Selector() string {
@@ -12,4 +14,12 @@ func (class Class) WithPseudoClass(pseudoClass PseudoClass) SelectorWithPseudoCl
 
 func (class Class) Style(properties ...Property) RuleSet {
 	return For(class).Set(properties...)
+}
+
+func (class Class) Child(childClass string) Class {
+	return Class(fmt.Sprintf("%v__%v", string(class), childClass))
+}
+
+func (class Class) Variant(variantClass string) Class {
+	return Class(fmt.Sprintf("%v--%v", string(class), variantClass))
 }
