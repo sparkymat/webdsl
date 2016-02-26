9e8408b4c2ae8ecefa689f4bbcf05b90d9a490c4
Ajith Hussain <csy0013@googlemail.com>
2016-02-26 13:59:27 +1100

    Updated test

--- sql/query_test.go
+++ sql/query_test.go
@@ -7,44 +7,41 @@ import (
 )
 
 func TestQuery(t *testing.T) {
-	people := Table("people")
+	user := Table("user")
 	account := Table("account")
-	avatar := Table("avatar")
+	accountStatus := Table("account_status")
 
-	query := Select(
-		people.AllColumns(),
-		account.Column("email"),
-		avatar.Column("url"),
-	).
-		From(people).
-		InnerJoin(account.Column("person_id"), people.Column("id")).
-		InnerJoin(avatar.Column("person_id"), people.Column("id")).
-		Where(
-		people.Column("first_name").Like("rob%").
-			Or(people.Column("last_name").Like("rob%"))).
-		Order(people.Column("first_name"), Ascending).
-		Limit(20)
+	query := Select(user.AllColumns()).
+		From(user).
+		InnerJoin(account.Column("user_id"), user.Column("id")). // InnerJoin() joins on the table in the first arg
+		Where(account.Column("id").In(
+			Select(accountStatus.Column("account_id")).
+				From(accountStatus).
+				Where(accountStatus.Column("status").InStringArray([]string{"PENDING", "ACTIVE"})),
+		)).
+		Order(user.Column("name").SortAscending()).
+		Limit(100)
 
 	expectedSQL := `
 SELECT
-  people.*,
-  account.email,
-  avatar.url
-FROM people
-INNER JOIN account ON account.person_id = people.id
-INNER JOIN avatar ON avatar.person_id = people.id
-WHERE (people.first_name LIKE 'rob%') OR (people.last_name LIKE 'rob%')
-ORDER BY people.first_name ASC
-LIMIT 20;
-`
-
-	pretty.Println("---")
-	pretty.Println(expectedSQL)
-	pretty.Println("---")
-	pretty.Println(query.ToSQL())
-	pretty.Println("---")
-
+  user.*
+FROM user 
+INNER JOIN account ON account.user_id = user.id
+WHERE account.id IN (
+SELECT
+  account_status.account_id
+FROM account_status 
+WHERE account_status.status IN ('PENDING', 'ACTIVE')
+)
+ORDER BY user.name ASC
+LIMIT 100
+;`
 	if expectedSQL != query.ToSQL() {
+		pretty.Println("--Expected--")
+		pretty.Println(expectedSQL)
+		pretty.Println("--Received--")
+		pretty.Println(query.ToSQL())
+		pretty.Println("---")
 		t.Fail()
 	}
 }
